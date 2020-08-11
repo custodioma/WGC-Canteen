2e1684cbad6bae97b7dbdbcb56dda83f382cb8a8
custodioma <51392666+custodioma@users.noreply.github.com>
2020-08-12 11:44:26 +1200

    Add files via upload
    I added the queries to filter the items and drinks in their respective tables

--- canteen_drinks.php
+++ canteen_drinks.php
@@ -1,13 +1,13 @@
 <?php
 
 $con = mysqli_connect("localhost", "custodioma", "bentsun82", "custodioma_canteen1");
-if(isset($_GET['drink'])){
+if (isset($_GET['drink'])) {
     $id = $_GET['drink'];
-}else{
+} else {
     $id = 1;
 }
 
-$this_drink_query = "SELECT PName, Cost, Calories, Stock, Description FROM drinks WHERE DrinkID = '" .$id ."'";
+$this_drink_query = "SELECT PName, Cost, Calories, Stock, Description FROM drinks WHERE DrinkID = '" . $id . "'";
 $this_drink_result = mysqli_query($con, $this_drink_query);
 $this_drink_record = mysqli_fetch_assoc($this_drink_result);
 
@@ -15,7 +15,9 @@ $all_drinks_query = "SELECT DrinkID, PName, Cost, Calories, Stock, Description F
 $all_drinks_result = mysqli_query($con, $all_drinks_query);
 
 $drinks_AtoZ = "SELECT * FROM drinks ORDER BY PName ASC";
-$drinks_low_to_high = "SELECT * FROM drinks ORDER BY Cost ASC";
+$drink_ZtoA = "SELECT * FROM drinks ORDER BY PName DESC";
+$drinks_price_low_to_high = "SELECT * FROM drinks ORDER BY Cost ASC";
+$drinks_price_high_to_low = "SELECT * FROM drinks ORDER BY Cost DESC";
 
 
 ?>
@@ -52,11 +54,11 @@ $drinks_low_to_high = "SELECT * FROM drinks ORDER BY Cost ASC";
         <main>
             <h2>Drink Information</h2>
             <?php
-            echo "<p> Item Name:". $this_drink_record['PName']."<br>";
-            echo "<p> Cost:".$this_drink_record['Cost']."<br>";
-            echo "<p> Calories:".$this_drink_record['Calories']."<br>";
-            echo "<p> Stock:".$this_drink_record['Stock']."<br>";
-            echo "<p> Description:".$this_drink_record['Description']."<br>";
+            echo "<p> Item Name:" . $this_drink_record['PName'] . "<br>";
+            echo "<p> Cost:" . $this_drink_record['Cost'] . "<br>";
+            echo "<p> Calories:" . $this_drink_record['Calories'] . "<br>";
+            echo "<p> Stock:" . $this_drink_record['Stock'] . "<br>";
+            echo "<p> Description:" . $this_drink_record['Description'] . "<br>";
 
             ?>
 
@@ -64,105 +66,180 @@ $drinks_low_to_high = "SELECT * FROM drinks ORDER BY Cost ASC";
 
         <main>
             <h2>Select another drink</h2>
-            <form name='drinks_form' id='drinks_form' method='get' action='drinks.php'>
+            <form name='drinks_form' id='drinks_form' method='get' action='canteen_drinks.php'>
                 <select id='drink' name='drink'>
                     <!--options-->
                     <?php
-                    while($all_drinks_record = mysqli_fetch_assoc($all_drinks_result)){
-                        echo "<option value = '".$all_drinks_record['DrinkID']."'>";
+                    while ($all_drinks_record = mysqli_fetch_assoc($all_drinks_result)) {
+                        echo "<option value = '" . $all_drinks_record['DrinkID'] . "'>";
                         echo $all_drinks_record['PName'];
-                        echo"</option>";
+                        echo "</option>";
                     }
                     ?>
                 </select>
                 <input type='submit' name='drinks_button' value='Show me the drink information'>
             </form>
 
-        <main>
-            <h2>Select a Drink</h2>
+            <main>
+                <h2>Search a Drink</h2>
 
-            <form action = "" method="post">
-                <input type="text" name='search'>
-                <input type="submit" name="submit" value="Search">
-            </form>
-        </main>
+                <form action="" method="post">
+                    <input type="text" name='search'>
+                    <input type="submit" name="submit" value="Search">
+                </form>
+            </main>
 
-        <?php
+            <?php
 
-        if(isset($_POST['search'])){
-            $search = $_POST['search'];
+            if (isset($_POST['search'])) {
+                $search = $_POST['search'];
 
-            $query1 = "SELECT * FROM drinks WHERE PName LIKE '%$search%'";
-            $query = mysqli_query($con, $query1);
-            $count = mysqli_num_rows($query);
+                $query1 = "SELECT * FROM drinks WHERE PName LIKE '%$search%'";
+                $query2 = "SELECT * FROM drinks WHERE Cost > 0";
+                $query = mysqli_query($con, $query1, $query2);
+                $count = mysqli_num_rows($query);
 
-            if($count == 0){
-                echo "There was no search results!";
-            }else{
+                if ($count == 0) {
+                    echo "There was no search results!";
+                } else {
 
-                while ($row = mysqli_fetch_array($query)) {
+                    while ($row = mysqli_fetch_array($query)) {
 
-                    echo $row ['PName'];
-                    echo"<br>";
+                        echo $row ['PName'];
+                        echo "<br>";
+                    }
                 }
             }
-        }
-        ?>
+            ?>
 
             <h2> Drinks Available</h2>
 
+            <p>Check out the full range of drinks available at the canteen below! You can find them up above in the select drink section to get more information. Information includes a brief product description and the dietary requirements the drink meets!
+            <br><br>Sort by:
+            </p>
+
             <form action="canteen_drinks.php" method="post">
                 <input type='submit' name='drinks_AtoZ' value="From A-Z">
-                <input type='submit' name='drinks_low_to_high' value="Low to High">
-            </form>
+                <input type='submit' name='drinks_ZtoA' value="From Z-A">
+                <input type='submit' name='drinks_price_low_to_high' value="Price Low to High">
+                <input type='submit' name='drinks_price_high_to_low' value="High to Low">
+                <input type='submit' name='in_stock' value="In Stock">
+                <input type='submit' name='out_stock' value="Out of Stock">
+            </
+                form>
 
 
-            <table style="width:100%">
+            <table style="width:75%">
                 <tr>
                     <th>Item Name</th>
                     <th>Cost</th>
                     <th>Calories</th>
+                    <th>Stock</th>
                 </tr>
                 <tr>
-                    <td></td>
                 </tr>
-            <?php
-            if(isset($_POST['drinks_AtoZ']))
-            {
-                $result=mysqli_query($con, "SELECT * FROM drinks ORDER BY PName ASC");
-                if(mysqli_num_rows($result)!=0)
-                {
-                    while($test = mysqli_fetch_array($result))
-                    {
-                        $id = $test['DrinkID'];
-                        echo"<tr>";
-                        echo "<td>". $test['PName']. "</td>";
-                        echo "<td>". $test['Cost']. "</td>";
-                        echo "<td>". $test['Calories']. "</td>";
-                        echo "</tr>";
+                <?php
+                if (isset($_POST['drinks_AtoZ'])) {
+                    $result = mysqli_query($con, "SELECT * FROM drinks ORDER BY PName ASC");
+                    if (mysqli_num_rows($result) != 0) {
+                        while ($test = mysqli_fetch_array($result)) {
+                            $id = $test['DrinkID'];
+                            echo "<tr>";
+                            echo "<td>" . $test['PName'] . "</td>";
+                            echo "<td>" . $test['Cost'] . "</td>";
+                            echo "<td>" . $test['Calories'] . "</td>";
+                            echo "<td>" . $test['Stock'] . "</td>";
+                            echo "</tr>";
+                        }
                     }
                 }
-            }
-            ?>
+                ?>
 
-            <?php
-            if(isset($_POST['drinks_low_to_high']))
-            {
-                $result=mysqli_query($con, "SELECT * FROM drinks ORDER BY Cost ASC");
-                if(mysqli_num_rows($result)!=0)
-                {
-                    while($test = mysqli_fetch_array($result))
-                    {
-                        $id = $test['DrinkID'];
-                        echo"<tr>";
-                        echo "<td>". $test['PName']. "</td>";
-                        echo "<td>". $test['Cost']. "</td>";
-                        echo "<td>". $test['Calories']. "</td>";
-                        echo "</tr>";
+                <?php
+                if (isset($_POST['drinks_ZtoA'])) {
+                    $result = mysqli_query($con, "SELECT * FROM drinks ORDER BY PName DESC");
+                    if (mysqli_num_rows($result) != 0) {
+                        while ($test = mysqli_fetch_array($result)) {
+                            $id = $test['DrinkID'];
+                            echo "<tr>";
+                            echo "<td>" . $test['PName'] . "</td>";
+                            echo "<td>" . $test['Cost'] . "</td>";
+                            echo "<td>" . $test['Calories'] . "</td>";
+                            echo "<td>" . $test['Stock'] . "</td>";
+                            echo "</tr>";
+                        }
                     }
                 }
-            }
-            ?>
+                ?>
+
+                <?php
+                if (isset($_POST['drinks_price_low_to_high'])) {
+                    $result = mysqli_query($con, "SELECT * FROM drinks ORDER BY Cost ASC");
+                    if (mysqli_num_rows($result) != 0) {
+                        while ($test = mysqli_fetch_array($result)) {
+                            $id = $test['DrinkID'];
+                            echo "<tr>";
+                            echo "<td>" . $test['PName'] . "</td>";
+                            echo "<td>" . $test['Cost'] . "</td>";
+                            echo "<td>" . $test['Calories'] . "</td>";
+                            echo "<td>" . $test['Stock'] . "</td>";
+                            echo "</tr>";
+                        }
+                    }
+                }
+                ?>
+
+
+                <?php
+                if (isset($_POST['drinks_price_high_to_low'])) {
+                    $result = mysqli_query($con, "SELECT * FROM drinks ORDER BY Cost DESC");
+                    if (mysqli_num_rows($result) != 0) {
+                        while ($test = mysqli_fetch_array($result)) {
+                            $id = $test['DrinkID'];
+                            echo "<tr>";
+                            echo "<td>" . $test['PName'] . "</td>";
+                            echo "<td>" . $test['Cost'] . "</td>";
+                            echo "<td>" . $test['Calories'] . "</td>";
+                            echo "<td>" . $test['Stock'] . "</td>";
+                            echo "</tr>";
+                        }
+                    }
+                }
+                ?>
+
+                <?php
+                if (isset($_POST['in_stock'])) {
+                    $result = mysqli_query($con, "SELECT * FROM drinks WHERE stock > 0");
+                    if (mysqli_num_rows($result) != 0) {
+                        while ($test = mysqli_fetch_array($result)) {
+                            $id = $test['DrinkID'];
+                            echo "<tr>";
+                            echo "<td>" . $test['PName'] . "</td>";
+                            echo "<td>" . $test['Cost'] . "</td>";
+                            echo "<td>" . $test['Calories'] . "</td>";
+                            echo "<td>" . $test['Stock'] . "</td>";
+                            echo "</tr>";
+                        }
+                    }
+                }
+                ?>
+
+                <?php
+                if (isset($_POST['out_stock'])) {
+                    $result = mysqli_query($con, "SELECT * FROM drinks WHERE stock = 0");
+                    if (mysqli_num_rows($result) != 0) {
+                        while ($test = mysqli_fetch_array($result)) {
+                            $id = $test['DrinkID'];
+                            echo "<tr>";
+                            echo "<td>" . $test['PName'] . "</td>";
+                            echo "<td>" . $test['Cost'] . "</td>";
+                            echo "<td>" . $test['Calories'] . "</td>";
+                            echo "<td>" . $test['Stock'] . "</td>";
+                            echo "</tr>";
+                        }
+                    }
+                }
+                ?>
             </table>
         </main>
     </div>
--- items.php
+++ items.php
@@ -1,21 +1,26 @@
 <?php
 
 $con = mysqli_connect("localhost", "custodioma", "bentsun82", "custodioma_canteen1");
-if(isset($_GET['item'])){
+if (isset($_GET['item'])) {
     $id = $_GET['item'];
-}else{
+} else {
     $id = 1;
 
 
 }
 
 
-$this_item_query = "SELECT PName, Cost, Calories, Stock, Description FROM items WHERE ItemID = '" .$id."'";
+$this_item_query = "SELECT PName, Cost, Calories, Stock, Description FROM items WHERE ItemID = '" . $id . "'";
 $this_item_result = mysqli_query($con, $this_item_query);
 $this_item_record = mysqli_fetch_assoc($this_item_result);
 
 $all_items_query = "SELECT ItemID, PName, Cost, Calories, Stock, Description FROM items";
 $all_items_result = mysqli_query($con, $all_items_query);
+
+$items_AtoZ = "SELECT * FROM items ORDER BY PName ASC";
+$items_ZtoA = "SELECT * FROM items ORDER BY PName DESC";
+$items_price__low_to_high = "SELECT * FROM items ORDER BY Cost ASC";
+$items_price_high_to_low = "SELECT * FROM items ORDER BY Cost DESC";
 
 ?>
 
@@ -36,7 +41,7 @@ $all_items_result = mysqli_query($con, $all_items_query);
 </div>
 
 
-    </div>
+</div>
 
 </div>
 
@@ -55,18 +60,200 @@ $all_items_result = mysqli_query($con, $all_items_query);
 </div>
 <div class="section">
     <div class="container">
-        <main>
-            <h2>Item Information</h2>
-            <?php
-            echo "<p> Item Name:". $this_item_record['PName']."<br>";
-            echo "<p> Cost:".$this_item_record['Cost']."<br>";
-            echo "<p> Calories:".$this_item_record['Calories']."<br>";
-            echo "<p> Stock:".$this_item_record['Stock']."<br>";
-            echo "<p> Description:".$this_item_record['Description']."<br>"
 
-            ?>
+        <h2>Item Information</h2>
+        <?php
+        echo "<p> Item Name:" . $this_item_record['PName'] . "<br>";
+        echo "<p> Cost:" . $this_item_record['Cost'] . "<br>";
+        echo "<p> Calories:" . $this_item_record['Calories'] . "<br>";
+        echo "<p> Stock:" . $this_item_record['Stock'] . "<br>";
+        echo "<p> Description:" . $this_item_record['Description'] . "<br>"
+
+        ?>
 
         </main>
+
+        <main>
+            <h2>Select another item</h2>
+            <form name='drinks_form' id='drinks_form' method='get' action='items.php'>
+                <select id='item' name='item'>
+                    <!--options-->
+                    <?php
+                    while ($all_items_record = mysqli_fetch_assoc($all_items_result)) {
+                        echo "<option value = '" . $all_items_record['ItemID'] . "'>";
+                        echo $all_items_record['PName'];
+                        echo "</option>";
+                    }
+                    ?>
+                </select>
+                <input type='submit' name='items_button' value='Show me the item information'>
+            </form>
+
+            <main>
+                <h2>Search an Item</h2>
+
+                <form action="" method="post">
+                    <input type="text" name='search'>
+                    <input type="submit" name="submit" value="Search">
+                </form>
+            </main>
+
+            <?php
+
+            if (isset($_POST['search'])) {
+                $search = $_POST['search'];
+
+                $query1 = "SELECT * FROM items WHERE PName LIKE '%$search%'";
+                $query = mysqli_query($con, $query1);
+                $count = mysqli_num_rows($query);
+
+                if ($count == 0) {
+                    echo "There was no search results!";
+                } else {
+
+                    while ($row = mysqli_fetch_array($query)) {
+
+                        echo $row ['PName'];
+                        echo "<br>";
+                    }
+                }
+            }
+            ?>
+            <h2> Items Available</h2>
+
+            <p>Check out the full range of items available at the canteen below! You can find them up above in the
+                select drink section to get more information. Information includes a brief product description and the
+                dietary requirements the item meets!
+                <br><br>Sort by:
+            </p>
+
+            <form action="items.php" method="post">
+                <input type='submit' name='items_AtoZ' value="From A-Z">
+                <input type='submit' name='items_ZtoA' value="From Z-A">
+                <input type='submit' name='item_price_low_to_high' value="Price Low to High">
+                <input type='submit' name='item_price_high_to_low' value="Price High to Low">
+                <input type='submit' name='in_stock' value="In Stock">
+                <input type='submit' name='out_stock' value="Out of Stock">
+            </form>
+
+
+            <table style="width:75%">
+                <tr>
+                    <th>Item Name</th>
+                    <th>Cost</th>
+                    <th>Calories</th>
+                    <th>Stock</th>
+                </tr>
+                <tr>
+                </tr>
+                <?php
+                if (isset($_POST['items_AtoZ'])) {
+                    $result = mysqli_query($con, "SELECT * FROM items ORDER BY PName ASC");
+                    if (mysqli_num_rows($result) != 0) {
+                        while ($test = mysqli_fetch_array($result)) {
+                            $id = $test['ItemID'];
+                            echo "<tr>";
+                            echo "<td>" . $test['PName'] . "</td>";
+                            echo "<td>" . $test['Cost'] . "</td>";
+                            echo "<td>" . $test['Calories'] . "</td>";
+                            echo "<td>" . $test['Stock'] . "</td>";
+                            echo "</tr>";
+                        }
+                    }
+                }
+                ?>
+
+                <?php
+                if (isset($_POST['items_ZtoA'])) {
+                    $result = mysqli_query($con, "SELECT * FROM items ORDER BY PName DESC");
+                    if (mysqli_num_rows($result) != 0) {
+                        while ($test = mysqli_fetch_array($result)) {
+                            $id = $test['ItemID'];
+                            echo "<tr>";
+                            echo "<td>" . $test['PName'] . "</td>";
+                            echo "<td>" . $test['Cost'] . "</td>";
+                            echo "<td>" . $test['Calories'] . "</td>";
+                            echo "<td>" . $test['Stock'] . "</td>";
+                            echo "</tr>";
+                        }
+                    }
+                }
+                ?>
+
+                <?php
+                if (isset($_POST['item_price_low_to_high'])) {
+                    $result = mysqli_query($con, "SELECT * FROM items ORDER BY Cost ASC");
+                    if (mysqli_num_rows($result) != 0) {
+                        while ($test = mysqli_fetch_array($result)) {
+                            $id = $test['ItemID'];
+                            echo "<tr>";
+                            echo "<td>" . $test['PName'] . "</td>";
+                            echo "<td>" . $test['Cost'] . "</td>";
+                            echo "<td>" . $test['Calories'] . "</td>";
+                            echo "<td>" . $test['Stock'] . "</td>";
+                            echo "</tr>";
+                        }
+                    }
+                }
+                ?>
+
+
+                <?php
+                if (isset($_POST['item_price_high_to_low'])) {
+                    $result = mysqli_query($con, "SELECT * FROM items ORDER BY Cost DESC");
+                    if (mysqli_num_rows($result) != 0) {
+                        while ($test = mysqli_fetch_array($result)) {
+                            $id = $test['ItemID'];
+                            echo "<tr>";
+                            echo "<td>" . $test['PName'] . "</td>";
+                            echo "<td>" . $test['Cost'] . "</td>";
+                            echo "<td>" . $test['Calories'] . "</td>";
+                            echo "<td>" . $test['Stock'] . "</td>";
+                            echo "</tr>";
+                        }
+                    }
+                }
+                ?>
+                
+                <?php
+                if (isset($_POST['in_stock'])) {
+                    $result = mysqli_query($con, "SELECT * FROM items WHERE stock > 0");
+                    if (mysqli_num_rows($result) != 0) {
+                        while ($test = mysqli_fetch_array($result)) {
+                            $id = $test['ItemID'];
+                            echo "<tr>";
+                            echo "<td>" . $test['PName'] . "</td>";
+                            echo "<td>" . $test['Cost'] . "</td>";
+                            echo "<td>" . $test['Calories'] . "</td>";
+                            echo "<td>" . $test['Stock'] . "</td>";
+                            echo "</tr>";
+                        }
+                    }
+                }
+                ?>
+
+                <?php
+                if (isset($_POST['out_stock'])) {
+                    $result = mysqli_query($con, "SELECT * FROM items WHERE stock = 0");
+                    if (mysqli_num_rows($result) != 0) {
+                        while ($test = mysqli_fetch_array($result)) {
+                            $id = $test['ItemID'];
+                            echo "<tr>";
+                            echo "<td>" . $test['PName'] . "</td>";
+                            echo "<td>" . $test['Cost'] . "</td>";
+                            echo "<td>" . $test['Calories'] . "</td>";
+                            echo "<td>" . $test['Stock'] . "</td>";
+                            echo "</tr>";
+                        }
+                    }
+                }
+                ?>
+            </table>
+        </main>
+    </div>
+</div>
+</body>
+</html>
 
 
 
